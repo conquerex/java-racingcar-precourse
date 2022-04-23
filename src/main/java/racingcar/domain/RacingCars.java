d1cbb812df0ee5eddba330da9dc67f0144bcb936
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2022-04-23 17:14:36 +0900

    feat: add car and race model

---
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+import racingcar.domain.car.Car;
+
+import java.util.List;
+
+public class RacingCars {
+
+    private List<Car> cars;
+
+    public RacingCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+}
---
+++ src/main/java/racingcar/domain/car/CarPosition.java
@@ -0,0 +1,17 @@
+package racingcar.domain.car;
+
+public class CarPosition {
+    private int position;
+
+    public CarPosition(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
